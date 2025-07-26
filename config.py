fc5fdab289ab67c6bbab81375dad631735910c2a
asvath <ashaasvathaman@gmail.com>
2025-07-26 16:56:50 -0400

    added config file with directory path names

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+# Base directory
+BASE_DIR = r'C:\Users\ashaa\OneDrive\Desktop\SmartTransit'
+RAW_DATA_DIR = os.path.join(BASE_DIR, 'data', 'raw')
+DOCS_DIR = os.path.join(RAW_DATA_DIR, 'docs')
+CODE_DESC_DIR = os.path.join(RAW_DATA_DIR, 'code_descriptions')
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+
+# File paths
+DELAY_DATA = os.path.join(RAW_DATA_DIR, 'delays', 'TTC Subway Delay Data since 2025.csv')
+VALID_STATIONS_LIST = os.path.join(DOCS_DIR, 'ttc_subway_stations.txt')
+VALID_STATIONS_WITH_LINECODES = os.path.join(DOCS_DIR, 'ttc_subway_stations_with_linecodes.txt')
+CODE_DESCRIPTIONS = os.path.join(CODE_DESC_DIR, 'Code Descriptions.csv')
+
+# Column names (in order) used for merging raw TTC delay data files
+REFERENCE_COLS_ORDERED = [
+    'Date', 'Time', 'Day', 'Station', 'Code',
+    'Min Delay', 'Min Gap', 'Bound', 'Line', 'Vehicle'
+]
