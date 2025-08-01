8e76ce460d2f173ae6edd2f8e38055d2938b2532
asvath <ashaasvathaman@gmail.com>
2025-08-01 12:04:14 -0400

    added docstring

--- pipelines/preprocess_dataframe.py
+++ pipelines/preprocess_dataframe.py
@@ -1,62 +1,84 @@
 import os
-
 import pandas as pd
 
+
 from utils import load_utils, clean_utils, log_utils
-from config import DATA_DIR, LOG_DIR
+from config import LOG_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR
 
-interim_dir = os.path.join(DATA_DIR, "interim")
-processed_dir = os.path.join(DATA_DIR, "processed")
-merged_file_name = os.path.join(interim_dir, "merged_unfiltered.csv")
-clean_file_name = os.path.join(processed_dir, "cleaned_delay_data.csv")
+"""
+Preprocesses TTC subway delay data.
 
-# # load the raw delay data
-# dfs, files_loaded = load_utils.load_raw_data_files()
-#
-# # merge the dataframes
-# df_merged = clean_utils.merge_delay_data(dfs, files_loaded)
-#
-# # save to interim folder
-# df_merged.to_csv(merged_file_name, index=False)
+This script performs the following:
+- Merges multiple raw delay data files
+- Removes invalid records (e.g., NaNs, zero-minute delays, missing vehicle numbers)
+- Cleans and standardizes key fields (e.g., station names, delay codes, bounds)
+- Adds helper columns such as station category and `is_weekend`
+- Saves the cleaned DataFrame to the processed data directory
 
-# load merged unfiltered data
-df = pd.read_csv(merged_file_name)
+For manual verification purposes, the script also logs:
+- Names of raw delay data files merged, along with any errors during merging (e.g., missing or extra columns)
+- Unique stations by category (passenger, non-passenger, unknown)
+- Station names containing directionals (e.g., "to", "towards", "toward")
+"""
 
-# drop nan data and data with no delay or no vehicle number
-df = clean_utils.drop_invalid_rows(df)
+merged_file_name = os.path.join(INTERIM_DATA_DIR, "merged_unfiltered.csv")
+clean_file_name = os.path.join(PROCESSED_DATA_DIR, "cleaned_delay_data.csv")
 
-# standardize station names
-df = clean_utils.clean_station_column(df)
+def clean_dataframe():
 
-# categorize stations into passenger, non-passenger and unknown
-df = clean_utils.add_station_category(df)
+    # load the raw delay data
+    dfs, files_loaded = load_utils.load_raw_data_files()
 
-# clean linecode
-df = clean_utils.clean_linecode(df)
+    # merge the dataframes
+    df_merged = clean_utils.merge_delay_data(dfs, files_loaded)
 
-# add datetime column
-df = clean_utils.clean_and_add_datetime(df)
+    # save to interim folder
+    df_merged.to_csv(merged_file_name, index=False)
 
-# Remove any rows where Date, Time, or DateTime have missing values after parsing
-df = df.dropna()
+    # load merged unfiltered data
+    df = pd.read_csv(merged_file_name)
 
-# clean day
-df = clean_utils.clean_day(df)
+    # drop nan data and data with no delay or no vehicle number
+    df = clean_utils.drop_invalid_rows(df)
 
-# add IsWeekday column
-df = clean_utils.add_IsWeekday(df)
+    # standardize station names
+    df = clean_utils.clean_station_column(df)
 
-# Clean bound
-df = clean_utils.clean_bound(df)
+    # categorize stations into passenger, non-passenger and unknown
+    df = clean_utils.add_station_category(df)
 
-# Remove any rows after cleaning data
-df = df.dropna()
+    # clean linecode
+    df = clean_utils.clean_linecode(df)
 
-# Log unique stations by category
-df = log_utils.log_unique_stations_by_category(df, LOG_DIR)
+    # add datetime column
+    df = clean_utils.clean_and_add_datetime(df)
 
-# Log stations which directional names
-df = log_utils.log_station_names_with_directionals(df, LOG_DIR)
+    # Remove any rows where Date, Time, or DateTime have missing values after parsing
+    df = df.dropna()
 
-# Write out cleaned csv
-df.to_csv(clean_file_name, index=False)
+    # clean day
+    df = clean_utils.clean_day(df)
+
+    # add IsWeekday column
+    df = clean_utils.add_IsWeekday(df)
+
+    # Clean bound
+    df = clean_utils.clean_bound(df)
+
+    # Remove any rows after cleaning data
+    df = df.dropna()
+
+    # Log unique stations by category
+    df = log_utils.log_unique_stations_by_category(df, LOG_DIR)
+
+    # Log stations which directional names
+    df = log_utils.log_station_names_with_directionals(df, LOG_DIR)
+
+    # Write out cleaned csv
+    df.to_csv(clean_file_name, index=False)
+
+    print(f"Cleaned and saved dataframe {clean_file_name}")
+
+
+if __name__=="__main()__":
+    clean_dataframe()
