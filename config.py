1b34d9fa567950c4841d6293af4a0bd8e0c7fca2
asvath <ashaasvathaman@gmail.com>
2025-08-01 11:35:46 -0400

    added processed data directory paths

--- config.py
+++ config.py
@@ -11,8 +11,11 @@ LOG_DIR = os.path.join(BASE_DIR, 'logs')
 # Data Directory
 DELAY_DATA_DIR = os.path.join(RAW_DATA_DIR, 'delays')
 
+# Processed data directories
+INTERIM_DATA_DIR = os.path.join(DATA_DIR, 'interim')
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, 'processed')
+
 # File paths
-DELAY_DATA = os.path.join(RAW_DATA_DIR, 'delays', 'TTC Subway Delay Data since 2025.csv')
 VALID_STATIONS_LIST = os.path.join(DOCS_DIR, 'ttc_subway_stations.txt')
 VALID_STATIONS_WITH_LINECODES = os.path.join(DOCS_DIR, 'ttc_subway_stations_with_linecodes.txt')
 CODE_DESCRIPTIONS = os.path.join(CODE_DESC_DIR, 'Code Descriptions.csv')
