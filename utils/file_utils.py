c21bc16faceca25510675bd65679093df25a5884
asvath <ashaasvathaman@gmail.com>
2025-08-06 17:29:32 -0400

    Added timestamp to write_to_csv, added read_csv function

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 """
 Utility functions for writing and saving various data outputs (e.g CSV) during TTC delay data preprocessing.
 """
-def write_to_csv(df:pd.DataFrame, prefix: str, output_dir:str) -> str:
+def write_to_csv(df:pd.DataFrame, prefix: str, output_dir:str, timestamped = True) -> str:
     """
     Saves a DataFrame to a CSV file with a timestamped filename.
 
@@ -15,7 +15,19 @@ def write_to_csv(df:pd.DataFrame, prefix: str, output_dir:str) -> str:
     :param output_dir: The directory where the file will be saved.
     :return: The full path to the saved CSV file.
     """
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    path = os.path.join(output_dir, f'{prefix}_{timestamp}.csv')
+    if timestamped:
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        path = os.path.join(output_dir, f'{prefix}_{timestamp}.csv')
+    else:
+        path = os.path.join(output_dir, f'{prefix}.csv')
     df.to_csv(path, index=False)
     return path
+
+def read_csv(filepath: str) -> pd.DataFrame:
+    """
+     Reads csv as pandas DataFrame.
+    :param filepath: Path to csv
+    :return: pd.DataFrame
+    """
+
+    return pd.read_csv(filepath)
