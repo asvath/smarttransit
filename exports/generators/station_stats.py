cc6a19e8854bc9b5967c14d7b2b95a1506dfb9ad
asvath <ashaasvathaman@gmail.com>
2025-09-08 16:29:11 -0400

    WIP track instrusion station stations

--- exports/generators/station_stats.py
+++ exports/generators/station_stats.py
@@ -45,3 +45,94 @@ def station_stats(df_year_station:pd.DataFrame, total_system_wide_delays_year, u
         "pct_of_system_delays_originating": round(percentage_of_delays_orig, 2),
         "top_reason_for_delays": top_reason_for_delays,
     }
+
+def track_intrusion_station_stats(df, code, top_n, unit: str = "minutes"):
+    """
+    Get the stations that are consistenly in the top N stations with track intrusions and priority one (train is in contact with patron) across different years
+    :param df: pd.DataFrame filtered by years (e.g 2023 to 2025)
+    :param unit:
+    :return:
+    """
+    # df = df[df["Code"].isin(["SUUT", "MUPR1"])]
+    df = df[df["Code"].isin(code)]
+
+
+    # conversion factor
+    factors = {
+        "minutes": 1,
+        "hours": 60,
+        "days": 60 * 24,
+    }
+
+    df["Year"] = df["DateTime"].dt.year
+    track_intrusion_stations = (
+        df.groupby(["Year", "Station"])
+        .size()
+        .reset_index(name="Count")
+    )
+
+    time_severity = (
+        df.groupby(["Station"])["Min Delay"]
+        .mean()
+        .reset_index(name="Avg Delay per Intrusion")
+    )
+
+    time_severity["Avg Delay per Intrusion"] = time_severity["Avg Delay per Intrusion"] / factors[unit]
+
+    count_severity = track_intrusion_stations.groupby(["Station"])["Count"].sum().reset_index(name="Total Count")
+    number_of_years = len(df["Year"].unique())
+    if number_of_years > 1:
+        current_year = df["Year"].max()
+        months = df.loc[df["Year"] == current_year, "DateTime"].dt.month.max()
+        total_no_of_mths = (number_of_years - 1) * 12 + months
+
+    else:
+        total_no_of_mths = number_of_years * 12
+
+    count_severity["Avg Count per Year"] = ((count_severity["Total Count"] / total_no_of_mths) * 12).round(1)
+
+    # take top n stations by year
+    top_n_stations_by_year =(
+        track_intrusion_stations.sort_values(["Year", "Count"], ascending = [True, False]).
+        groupby("Year").head(top_n))
+
+    # Top N stations in a year as a set e.g {Bloor-Yonge, Spadina, St. George}, where year is the index
+    station_sets = (
+        top_n_stations_by_year.groupby("Year")
+                    ["Station"].apply(set)) # e.g 2018
+
+
+
+    stations_list_of_sets = station_sets.tolist() # e.g [{Bloor-Yonge, Spadina}, {Bloor-Yonge, Rosedale}]
+
+    consistent_stations = set.intersection(*stations_list_of_sets)
+
+    if not consistent_stations:
+        return []
+
+
+
+    consistent_stations_stats = {}
+    for s in consistent_stations:
+        station_stats = {}
+        time_stats = time_severity[time_severity["Station"] == s]
+        count_stats = count_severity[count_severity["Station"] == s]
+
+        station_stats["Avg Delay per Intrusion"] = time_stats["Avg Delay per Intrusion"].iloc[0].round(2)
+        station_stats["Avg Count per Year"] = count_stats["Avg Count per Year"].iloc[0].round(2)
+        station_stats["Avg Time Lost per Year (hours)"] =\
+            ((station_stats["Avg Delay per Intrusion"] * station_stats["Avg Count per Year"])
+             / factors["hours"]).round(2)
+        consistent_stations_stats[s] = station_stats
+
+
+    return consistent_stations_stats
+
+
+from utils.ttc_loader import TTCLoader
+loader = TTCLoader()
+
+df = loader.filter_selected_years(2023,2025).df
+# print(track_intrusion_station_stats(df, ["SUUT", "MUPR1"],10, "minutes"))
+print(track_intrusion_station_stats(df, ["SUDP"],10, "minutes"))
+
