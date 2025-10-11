fc42ddcabe838c67c5b782628f26dd0bb805e1ee
asvath <ashaasvathaman@gmail.com>
2025-10-10 22:09:29 -0400

    cleaned up functions and added for major delay

--- exports/generators/generate_plots.py
+++ exports/generators/generate_plots.py
@@ -1,7 +1,8 @@
 from viz.eda_plots import (plot_total_delay_by_year, plot_delay_category_trend_by_year,
                            plot_delay_description_trend_by_year, plot_line_trends_by_year, plot_station_trend_by_year,
-                           plot_consistently_top_station_trend, plot_rush_hour_trends_by_year,
-                           plot_season_trends_by_year, plot_major_delay_trend, plot_minor_delay_trend)
+                           plot_rush_hour_trends_by_year,
+                           plot_season_trends_by_year, plot_major_delay_trend, plot_minor_delay_trend,
+                           plot_weekday_weekend_trends_by_year, plot_delay_category_trend_for_major_delay)
 from viz.eda_utils import fig_to_html
 from utils.ttc_loader import TTCLoader
 from config import EXPORTS_PLOTS_DIR
@@ -67,6 +68,18 @@ fig_to_html(fig, EXPORTS_PLOTS_DIR, "disorderly_patron_station_delay_trends_by_y
 # fire on track level trend
 loader.clear_filters()
 df = loader.filter_delay_code("MUPLB").df
-fig = plot_station_trend_by_year(df=df, unit="hours",
+fig = plot_station_trend_by_year(df=df, unit="hours", top_n=10,
                                  title=f"TTC Delays (Fire on track level): Top stations by Hours Lost per Year")
 fig_to_html(fig, EXPORTS_PLOTS_DIR, "fire_on_track_level_station_delay_trends_by_year")
+
+# weekday vs. weekend
+loader.clear_filters()
+df = loader.df
+fig = plot_weekday_weekend_trends_by_year(df=df)
+fig_to_html(fig, EXPORTS_PLOTS_DIR, "weekday_weekend_delay_trends_by_year")
+
+# delay trends for major delay
+fig = plot_delay_category_trend_for_major_delay(df = df, unit = "days",
+                                                title = f"TTC Delay: Top Delay Categories by Days Lost"
+                                                        f" per Year (>=20 min)")
+fig_to_html(fig, EXPORTS_PLOTS_DIR, "delay_category_trend_for_major_delays_by_year")
